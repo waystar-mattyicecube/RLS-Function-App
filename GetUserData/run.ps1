dadf5ead37be7057817da18180c37b145bd0f8f5
waystar-mattyicecube <matthew.elliott@waystar.com>
2024-10-20 23:21:46 -0400

    Update run.ps1

--- GetUserData/run.ps1
+++ GetUserData/run.ps1
@@ -9,10 +9,6 @@ $response = Invoke-RestMethod -Method Get -Uri $graphApiUrl -Headers @{
 # List the users from the response
 $users = $response.value
 
-# Example of fetching custom security attributes (assumed as extension properties)
-# Replace 'customSecurityAttribute1' and 'customSecurityAttribute2' with the actual attribute names if they're stored as extensions or directory schema extensions
-$customAttributesUrl = "https://graph.microsoft.com/v1.0/users/{user_id}?`$select=extension_customSecurityAttribute1,extension_customSecurityAttribute2"
-
 foreach ($user in $users) {
     Write-Host "User Principal Name: $($user.userPrincipalName)"
     Write-Host "Display Name: $($user.displayName)"
@@ -25,14 +21,13 @@ foreach ($user in $users) {
     Write-Host "Created DateTime: $($user.createdDateTime)"
     Write-Host "Last Password Change: $($user.lastPasswordChangeDateTime)"
 
-    # Query for custom attributes
-    $customResponse = Invoke-RestMethod -Method Get -Uri $customAttributesUrl.Replace("{user_id}", $user.id) -Headers @{
+    # Query for custom attributes (EntraAccessLevel as an example)
+    $customAttributesUrl = "https://graph.microsoft.com/v1.0/users/$($user.id)?`$select=extension_EntraAccessLevel"
+    $customResponse = Invoke-RestMethod -Method Get -Uri $customAttributesUrl -Headers @{
         Authorization = "Bearer $($token)"
     }
 
-    $customSecurityAttr1 = $customResponse.extension_customSecurityAttribute1
-    $customSecurityAttr2 = $customResponse.extension_customSecurityAttribute2
-
-    Write-Host "Custom Security Attribute 1: $customSecurityAttr1"
-    Write-Host "Custom Security Attribute 2: $customSecurityAttr2"
+    # Output custom attribute values
+    $EntraAccessLevel = $customResponse.extension_EntraAccessLevel
+    Write-Host "EntraAccessLevel: $EntraAccessLevel"
 }
